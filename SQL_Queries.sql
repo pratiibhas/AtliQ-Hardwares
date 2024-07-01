c762127737948529e926553f04333330798c2092
Pratibha Singh <120117087+pratiibhas@users.noreply.github.com>
2024-07-01 20:20:24 +0530

    Update SQL_Queries.sql

--- SQL_Queries.sql
+++ SQL_Queries.sql
@@ -248,7 +248,7 @@ SELECT
 FROM 
     Channel_sales cs, Total_Sales ts;
 
-/*Get the Top 3 products in each division that have a high
+/*10. Get the Top 3 products in each division that have a high
 total_sold_quantity in the fiscal_year 2021? The final output contains these
 fields,
 division
